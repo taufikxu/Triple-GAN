3b3d841d4b1cff9aafe9b4a143e5be78b994f266
zhenxuan00 <chongxuanli1991@gmail.com>
2020-06-17 12:29:47 +0800

    add mt

--- command.sh
+++ command.sh
@@ -15,4 +15,8 @@ python train_triplegan.py ./configs/triple_gan_svhn.yaml -dataset svhn -key fix_
 python train_triplegan.py ./configs/triple_gan_svhn.yaml -dataset svhn -key cford_int -gpu 1
 
 
+python train_triplegan_mt_svhn.py ./configs/triple_gan_mt_svhn.yaml -dataset svhn -subfolder tganmt -translate 0 -gpu 0
+python train_triplegan_mt_svhn.py ./configs/triple_gan_mt_svhn.yaml -dataset svhn -subfolder tganmt -translate 2 -gpu 1
+
+
 python test_triplegan.py ./configs/triple_gan_svhn.yaml -dataset svhn -key test -old_model "/home/kunxu/Workspace/Triple-GAN/allresults/(train_triplegan.py)_(svhn)_(2020-06-12-11-24-49)_()_(cford_int)/models/model209000.pt" -gpu 1
