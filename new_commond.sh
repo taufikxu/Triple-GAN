12583ff4d4abf8f725f7bee83d9c549390ac9318
zhenxuan00 <chongxuanli1991@gmail.com>
2020-08-15 22:18:59 +0800

    bcr new tgan results in new command. all together

--- new_commond.sh
+++ new_commond.sh
@@ -1,13 +1,3 @@
-# 写好程序写好命令，在一个机器上确性能跑，然后不断杀。
-
-# 杀完清理下文件
-
-# 留一些机器给xuan跑。这样随时可以看。
-
-# 其实就是elr的结果。只是三个数据一起调整比较麻烦。
-
-# 前往别忘记 push 和 pull
-
 # - test bcr on ssl tiny tgan (to generate images and check bcr for bug and check elr for bug)
     
     # xuan 
@@ -21,10 +11,22 @@
     # should compare with losses in ssl settings to debug or improve
 
     # NEW_SVHN g36
-    python train_triplegan.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 0.3 -n_labels 1000 -translate 2 -gpu 0
-    python train_triplegan.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 1.0 -n_labels 1000 -translate 2 -gpu 0
-    python train_triplegan.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 1.0 -n_labels 1000 -pdl_ramp_start 40000 -pdl_ramp_end 60000 -translate 2 -gpu 3
-    python train_triplegan.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 1.0 -n_labels 1000 -translate 0 -gpu 3
-    
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 0.3 -n_labels 1000 -translate 2 -gpu 1
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 1.0 -n_labels 1000 -translate 2 -gpu 2
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 1.0 -n_labels 1000 -pdl_ramp_start 40000 -pdl_ramp_end 60000 -translate 2 -gpu 3
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 1.0 -n_labels 1000 -translate 0 -gpu 4
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_SVHN -alpha_c_pdl 3.0 -n_labels 1000 -translate 2 -gpu 7
 
-    # cifar10
+    # NEW_CIFAR10 g32
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 3.0 -n_labels 4000 -ssl_seed 1001 -translate 2 -flip_horizontal true -gpu 0
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 1.0 -n_labels 4000 -ssl_seed 1001 -translate 2 -flip_horizontal true -gpu 1
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 3.0 -n_labels 4000 -ssl_seed 1001 -translate 0 -flip_horizontal false -gpu 2
+
+    # NEW_TINY g
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 3.0 -n_labels 2000 -ssl_seed 1001 -translate 2 -flip_horizontal true -gpu 0
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 1.0 -n_labels 2000 -ssl_seed 1001 -translate 2 -flip_horizontal true -gpu 1
+    python train_triplegan_final_elr.py ./configs/triple_gan_svhn_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 3.0 -n_labels 2000 -ssl_seed 1001 -translate 0 -flip_horizontal false -gpu 2
+
+    python train_triplegan_final_elr.py ./configs/triple_gan_tinyimagenet_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 3.0 -n_labels 2000 -ssl_seed 1001 -translate 2 -flip_horizontal true -gpu 0
+    python train_triplegan_final_elr.py ./configs/triple_gan_tinyimagenet_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 1.0 -n_labels 2000 -ssl_seed 1001 -translate 2 -flip_horizontal true -gpu 1
+    python train_triplegan_final_elr.py ./configs/triple_gan_tinyimagenet_noaug_elr.yaml -subfolder NEW_CIFAR10 -alpha_c_pdl 3.0 -n_labels 2000 -ssl_seed 1001 -translate 0 -flip_horizontal false -gpu 2
